685800c5d1d7271702fcf76cc2c69507209e9099
Anavatis <44580637+Anavatis@users.noreply.github.com>
2022-02-10 13:54:59 +0300

    Edit wsgi file

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,11 @@
+import site
+import sys
+
+sys.path.insert(0, '/var/www/www-root/data/www/kamasutra')
+site.addsitedir('/var/www/www-root/data/www/kamasutra/env/lib/python3.8/site-packages')
+
+from flup.server.fcgi import WSGIServer
 from main import app
 
 if __name__ == '__main__':
-    app.run()
+    WSGIServer(app).run()
